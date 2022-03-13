4c6a3a61cb87d4ebcb28e326054e8003243a7e75
Ankit-Lodh <ankit.intellegencesystem2019@gmail.com>
2022-03-13 13:13:30 +0530

    redux thunk

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -5,6 +5,7 @@ import {
 } from "reactstrap";
 import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
+import { Loading } from './LoadingComponent';
 
 const required = (val) => val && val.length; //value > 0
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
@@ -44,30 +45,30 @@ class CommentForm extends Component {
                             <Row className="form-group m-1">
                                 <Label htmlFor="rating"  >Rating</Label>
 
-                                    <Control.select model=".rating"
-                                        className="form-control"
-                                        name="rating"
-                                        id="rating"
-                                        validators={{
-                                            required
-                                        }}
-                                    >
-                                        <option>Please Select</option>
-                                        <option>1</option>
-                                        <option>2</option>
-                                        <option>3</option>
-                                        <option>4</option>
-                                        <option>5</option>
-                                    </Control.select>
-                                    <Errors
-                                        className="text-danger"
-                                        model=".author"
-                                        show="touched"
-                                        messages={{
-                                            required: 'Required',
-                                        }}
-                                    />
- 
+                                <Control.select model=".rating"
+                                    className="form-control"
+                                    name="rating"
+                                    id="rating"
+                                    validators={{
+                                        required
+                                    }}
+                                >
+                                    <option>Please Select</option>
+                                    <option>1</option>
+                                    <option>2</option>
+                                    <option>3</option>
+                                    <option>4</option>
+                                    <option>5</option>
+                                </Control.select>
+                                <Errors
+                                    className="text-danger"
+                                    model=".author"
+                                    show="touched"
+                                    messages={{
+                                        required: 'Required',
+                                    }}
+                                />
+
                             </Row>
                             <Row className="form-group m-1">
                                 <Label htmlFor="firstName">First Name</Label>
@@ -132,7 +133,7 @@ function RenderDish({ dish }) {
     }
 }
 
-function RenderComments({ comments  ,addComment, dishId}) {
+function RenderComments({ comments, addComment, dishId }) {
     if (comments == null) {
         return (<div></div>)
     }
@@ -165,42 +166,55 @@ function RenderComments({ comments  ,addComment, dishId}) {
 
 
 const DishDetail = (props) => {
-
-    const dish = props.dish
-
-
-    if (dish == null) {
-        return (<div></div>);
-    }
-
-    return (
-        <div className="container">
-            <div className="row">
-                <Breadcrumb>
-                    <BreadcrumbItem>
-                        <Link to="/menu">Menu</Link>
-                    </BreadcrumbItem>
-                    <BreadcrumbItem active>
-                        {props.dish.name}
-                    </BreadcrumbItem>
-                </Breadcrumb>
-
-                <div className="col-12">
-                    <h3> {props.dish.menu}</h3>
-                    <hr />
+    if (props.isLoading) {
+        return (
+            <div className="container">
+                <div className="row">
+                    <Loading />
                 </div>
             </div>
-
-            <div className='row'>
-                <RenderDish dish={props.dish} />
-                <RenderComments comments={props.comments} 
-                addComment={props.addComment}
-                dishId={props.dish.id}
-                />
-                
+        );
+    }
+    else if (props.errMess) {
+        return (
+            <div className="container">
+                <div className="row">
+                    <h4>{props.errMess}</h4>
+                </div>
             </div>
-        </div>
-    )
+        );
+    }
+    else if (props.dish != null) {
+
+        return (
+            <div className="container">
+                <div className="row">
+                    <Breadcrumb>
+                        <BreadcrumbItem>
+                            <Link to="/menu">Menu</Link>
+                        </BreadcrumbItem>
+                        <BreadcrumbItem active>
+                            {props.dish.name}
+                        </BreadcrumbItem>
+                    </Breadcrumb>
+
+                    <div className="col-12">
+                        <h3> {props.dish.menu}</h3>
+                        <hr />
+                    </div>
+                </div>
+
+                <div className='row'>
+                    <RenderDish dish={props.dish} />
+                    <RenderComments comments={props.comments}
+                        addComment={props.addComment}
+                        dishId={props.dish.id}
+                    />
+
+                </div>
+            </div>
+        )
+    }
 }
 
 
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,32 +1,53 @@
 import React from "react";
-import { Card, CardImg, CardImgOverlay, CardTitle, Breadcrumb, BreadcrumbItem  } from "reactstrap";
-import {Link} from 'react-router-dom';
-     
+import { Card, CardImg, CardImgOverlay, CardTitle, Breadcrumb, BreadcrumbItem } from "reactstrap";
+import { Link } from 'react-router-dom';
+import { Loading } from './LoadingComponent';
 
-    function RenderMenuItem({ dish, onClick }) {
-        return(
-            <Card>
-                <Link to={ `/menu/${dish.id}` } >
-                    <CardImg width="100%" src={dish.image} alt={dish.name} />
-                    <CardImgOverlay>
-                        <CardTitle> {dish.name}</CardTitle>
-                    </CardImgOverlay>
-                </Link>
-            </Card>   
+
+function RenderMenuItem({ dish, onClick }) {
+    return (
+        <Card>
+            <Link to={`/menu/${dish.id}`} >
+                <CardImg width="100%" src={dish.image} alt={dish.name} />
+                <CardImgOverlay>
+                    <CardTitle> {dish.name}</CardTitle>
+                </CardImgOverlay>
+            </Link>
+        </Card>
+    );
+}
+
+const Menu = (props) => {
+
+    const menu = props.dishes.dishes.map((dish) => {
+        return (
+            <div key={dish.id} className="col-12 col-md-5 m-1">
+                <RenderMenuItem dish={dish} />
+            </div>
+        );
+    });
+    if (props.dishes.isLoading) {
+        return (
+            <div className="container">
+                <div className="row">
+                    <Loading />
+                </div>
+            </div>
         );
     }
-
-    const Menu = (props) => {
-
-        const menu = props.dishes.map((dish) => {
-            return (
-                <div key={ dish.id } className="col-12 col-md-5 m-1">    
-                    <RenderMenuItem dish={dish}  />
+    else if (props.dishes.errMess) {
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col-12">
+                        <h4>{props.dishes.errMess}</h4>
+                    </div>
                 </div>
-            );
-        });
-
-        return(
+            </div>
+        );
+    }
+    else {
+        return (
             <div className="container">
                 <div className="row">
                     <Breadcrumb>
@@ -37,7 +58,7 @@ import {Link} from 'react-router-dom';
                             Menu
                         </BreadcrumbItem>
                     </Breadcrumb>
-                    
+
                     <div className="col-12">
                         <h3>Menu</h3>
                         <hr />
@@ -45,13 +66,14 @@ import {Link} from 'react-router-dom';
                 </div>
 
                 <div className="row">
-                    { menu }
+                    {menu}
                 </div>
             </div>
         );
-
     }
-    
+
+}
+
 
 
 
